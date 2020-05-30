c6d42445f9678b9ca29ff8765f981bb0ca2fbcfb
Arnaud Lefebvre <a.lefebvre@outlook.fr>
2020-05-30 02:28:01 +0200

    Alarm: expose code constants
    Needed in the UI

--- src/alarm.rs
+++ src/alarm.rs
@@ -3,16 +3,16 @@
 // Copyright: 2020, Makers For Life
 // License: Public Domain License
 
-const RMC_SW_1: u8 = 12;
-const RMC_SW_2: u8 = 11;
-const RMC_SW_3: u8 = 14;
-const RMC_SW_11: u8 = 21;
-const RMC_SW_12: u8 = 13;
-const RMC_SW_14: u8 = 22;
-const RMC_SW_15: u8 = 23;
-const RMC_SW_16: u8 = 31;
-const RMC_SW_18: u8 = 17;
-const RMC_SW_19: u8 = 24;
+pub const RMC_SW_1: u8 = 12;
+pub const RMC_SW_2: u8 = 11;
+pub const RMC_SW_3: u8 = 14;
+pub const RMC_SW_11: u8 = 21;
+pub const RMC_SW_12: u8 = 13;
+pub const RMC_SW_14: u8 = 22;
+pub const RMC_SW_15: u8 = 23;
+pub const RMC_SW_16: u8 = 31;
+pub const RMC_SW_18: u8 = 17;
+pub const RMC_SW_19: u8 = 24;
 
 /// Wrapper arround an alarm code
 #[derive(Debug, Eq, PartialEq, Hash, Clone, Copy)]

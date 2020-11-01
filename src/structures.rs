7e59f2bcbafb1276526f78eb8918827e93a6d251
David Sferruzza <david.sferruzza@gmail.com>
2020-11-01 18:26:20 +0100

    Improve documentation

--- src/structures.rs
+++ src/structures.rs
@@ -165,9 +165,9 @@ pub struct DataSnapshot {
     pub blower_rpm: u8,
     /// Current battery level in volts
     pub battery_level: u8,
-    /// [protocol v2] Inspiratory flow
+    /// [protocol v2] Inspiratory flow in SLM * 100
     pub inspiratory_flow: Option<i16>,
-    /// [protocol v2] Expiratory flow
+    /// [protocol v2] Expiratory flow in SLM * 100
     pub expiratory_flow: Option<i16>,
 }
 

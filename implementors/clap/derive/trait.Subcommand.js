d3c3351388a549c3bdb99678d4dfc7ec5a9f41c5
dsferruzza <david.sferruzza@gmail.com>
2022-06-08 07:06:46 +0000

    Deploying to gh-pages from  @ 7040a1506d7e888ac66d9972db8657ce6c3bd895 🚀

--- implementors/clap/derive/trait.Subcommand.js
+++ implementors/clap/derive/trait.Subcommand.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
+implementors["clap"] = [];
 implementors["makair_telemetry_cli"] = [{"text":"impl <a class=\"trait\" href=\"clap/derive/trait.Subcommand.html\" title=\"trait clap::derive::Subcommand\">Subcommand</a> for <a class=\"enum\" href=\"makair_telemetry_cli/enum.Mode.html\" title=\"enum makair_telemetry_cli::Mode\">Mode</a>","synthetic":false,"types":["makair_telemetry_cli::Mode"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- tungstenite/handshake/client/sidebar-items.js
+++ tungstenite/handshake/client/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["ClientHandshake","Client handshake role."]],"type":[["Request","Client request type."],["Response","Client response type."]]});
+initSidebarItems({"fn":[["generate_key","Generate a random key for the `Sec-WebSocket-Key` header."]],"struct":[["ClientHandshake","Client handshake role."]],"type":[["Request","Client request type."],["Response","Client response type."]]});
